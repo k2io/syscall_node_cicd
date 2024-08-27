3d21ec2ccba0e60dcc8a6604fbc5f5c0d7cfea94
kkhandelwal-nr <kkhandelwal@newrelic.com>
2024-08-27 19:49:01 +0530

    Update startUpScript.sh

--- startUpScript.sh
+++ startUpScript.sh
@@ -23,6 +23,14 @@ else
 fi
 
 # command to copy newrelic.js from node_modules to application directory and configure fields in config file
+sleep 3s
+
+sed -i "s/'My Application'/'${NEW_RELIC_APP_NAME}'/g" /syscall_node/newrelic.js
+sed -i "s/'license key here'/'${NEW_RELIC_LICENSE_KEY}'/g" /syscall_node/newrelic.js
+sed -i "/license_key:/a\  host: 'staging-collector.newrelic.com'," /syscall_node/newrelic.js
+
+sed -i "/allow_all_headers:/a\  security: {\n    agent: {\n      enabled: true \n     },\n    enabled: true,\n    validator_service_url: 'wss://iast3-csec-se.test-poised-pear.cell.us.nr-data.net',\n   }," /syscall_node/newrelic.js
+
 
 # start Ldap Server
 node ldapServer.js & 
