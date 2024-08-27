814355d387b2bf648482f9c0d0b998f8333c1a9e
kishan-k2io <59822282+kishan-k2io@users.noreply.github.com>
2024-08-27 13:06:19 +0530

    Update startUpScript.sh

--- startUpScript.sh
+++ startUpScript.sh
@@ -1,17 +1,3 @@
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-# Node installation
-if [ -z "$NODE_VERSION" ]
-then
-    echo "Node version is not set, switching to default version 18.16.1"
-    nvm install 18.16.1
-    npm install --save
-else
-    nvm install $NODE_VERSION
-    npm install --save
-fi
-
 # Start mysql
 chown -R mysql:mysql /var/lib/mysql
 service mysql start
@@ -26,19 +12,6 @@ mysql -u root K2test < database/users.sql
 echo "Sleeping 10 sec for Mongo startup..."
 sleep 10s
 
-# Config file mounting
-cp $NEW_RELIC_SECURITY_CONFIG_PATH /syscall_node/
-
-# Agent installation
-if [ -z "$APM_BRANCH" ]
-then
-    echo "APM_BRANCH is not set, installing ${APM_VERSION} APM agent"
-    npm install newrelic@${APM_VERSION}
-else
-    echo "https://github.com/newrelic/node-newrelic.git/#${APM_BRANCH}"
-    npm install "https://github.com/newrelic/node-newrelic.git/#${APM_BRANCH}"
-fi
-
 if [ -z "$CSEC_BRANCH" ]
 then
     echo "CSEC_BRANCH is not set"
@@ -48,6 +21,8 @@ else
     npm install "https://github.com/newrelic/csec-node-agent.git/#${CSEC_BRANCH}"
     rm -rf node_modules/newrelic/node_modules/@newrelic/security-agent
 fi
+
+# command to copy newrelic.js from node_modules to application directory and configure fields in config file
 
 # start Ldap Server
 node ldapServer.js & 
